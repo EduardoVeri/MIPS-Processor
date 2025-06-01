5a92c17d8f81ca60c7a43d92376567ca1eeb7a74
eduardoveri <verissimo.eduardo@unifesp.br>
2025-06-01 14:15:53 -0300

    Refactor Entrada module to improve keyboard handling and streamline input processing

--- Entrada.v
+++ Entrada.v
@@ -5,23 +5,43 @@ module Entrada (
     input wire ps2_clk,
     input wire ps2_data,
     input Pause,
+    input [1:0] In,
     output [13:0] resultadoEntrada,
+    output reg [7:0] resultadoKeyBoard,
     output saidaBotao,
     saidaClock
 );
 
-  reg     [25:0] out;
-  reg     [12:0] resultado;
-  reg            RegClock;
-  reg     [ 5:0] Debouncer;
-  integer        contadorDebouncer = 1;
-  integer        contador = 0;
+  wire [7:0] ps2_keyboard_data;
+  reg [7:0] KeyBoardBuffer;
+  reg [25:0] out;
+  reg [12:0] resultado;
+  reg RegClock;
+  reg [5:0] Debouncer;
+  reg data_valid;
+  integer contadorDebouncer = 1;
+  integer contador = 0;
+
+  initial begin
+    KeyBoardBuffer = 8'd0;
+    resultadoKeyBoard = 8'd0;
+    out = 26'd0;
+    resultado = 14'd0;
+    RegClock = 1'b0;
+    Debouncer = 6'd0;
+  end
+
+  PS2Key i_ps2Key (
+      .clk(Clock),
+      .PS2_clk(ps2_clk),
+      .PS2_DAT(ps2_data),
+      .data(ps2_keyboard_data),
+      .data_valid(data_valid)
+  );
 
   always @(posedge Clock) begin
-
     if ((Botao == 0) && (Debouncer[5] != 1)) Debouncer = Debouncer + 1;
     else if (Botao == 1) Debouncer = 6'd0;
-
   end
 
   always @(posedge Clock) begin
@@ -46,21 +66,25 @@ module Entrada (
     end
   end
 
-  //	always @(*) begin
-  //		if (Sw[13] == 1)
-  //			resultado = {1'd0, Sw[12:0]};
-  //	end
+  always @(*) begin
+    if (Sw[13] == 1) resultado = {1'd0, Sw[12:0]};
+  end
 
+  always @(posedge Clock) begin
+    if (data_valid == 1'b1) begin
+      KeyBoardBuffer <= ps2_keyboard_data;
+    end
 
-  PS2Key i_ps2Key (
-      .clk(Clock),
-      .PS2_clk(ps2_clk),
-      .PS2_DAT(ps2_data),
-      .data(resultadoEntrada)
-  );
+    if (In == 2'b10) begin
+      resultadoKeyBoard <= KeyBoardBuffer;
+      KeyBoardBuffer <= 8'd0;
+    end else begin
+      resultadoKeyBoard <= 8'd0;
+    end
+  end
 
   assign saidaBotao = Debouncer[5];
   assign saidaClock = RegClock;
-  //	assign resultadoEntrada = resultado;
+  assign resultadoEntrada = resultado;
 
 endmodule
