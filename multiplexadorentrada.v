703a8425cbe8e6a7758a2fa54242e9d3f21e5722
eduardoveri <verissimo.eduardo@unifesp.br>
2025-05-31 21:32:55 -0300

    Add KeyboardInput handling to MultiplexadorEntrada module

--- multiplexadorentrada.v
+++ multiplexadorentrada.v
@@ -1,7 +1,8 @@
 module MultiplexadorEntrada (
 	input [13:0] DadoLido_Entrada, 
 	input [31:0] Dado_MemoriaULA,
-	input In,
+	input [7:0] KeyboardInput,
+	input [1:0] In, // 0 - Dado_MemoriaULA, 1 - DadoLido_Entrada, 2 - KeyboardInput,
 	output [31:0] Escolhido_MultiplexadorEntrada
 );
 	
@@ -12,7 +13,9 @@ module MultiplexadorEntrada (
 		if(In == 1) begin
 			escolhido = {18'd0, DadoLido_Entrada};
 		end
-		
+		else if (In == 2) begin
+			escolhido = {24'd0, KeyboardInput};
+		end
 		else begin
 			escolhido = Dado_MemoriaULA;
 		end
